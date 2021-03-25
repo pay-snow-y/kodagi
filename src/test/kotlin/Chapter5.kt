072b6982922852eacf6fe6d693c61561c5a98e61
snow.y <snow.y@kakaopaycorp.com>
2021-03-26 01:12:37 +0900

    Chapter 5-6

--- src/test/kotlin/Chapter5.kt
+++ src/test/kotlin/Chapter5.kt
@@ -11,28 +11,36 @@ class Chapter5 {
 
     @Test
     fun `5-3`() {
+        println(List.Cons(1, List(2, 3, 4, 5)).drop(2))
+    }
+
+    @Test
+    fun `5-5`() {
+        // 리스트의 마지막 원소를 제거하는 함수를 작성하라
+        // fun init(): List<A>
+        println(List.Cons(1, List(2, 3, 4, 5)).init())
     }
 }
 
-sealed class List<A> {
-    abstract fun isEmpty(): Boolean
-    fun cons(newItem: A): List<A> = Cons(newItem, this)
+//fun sum(intList: List<Int>): Int = when (intList) {
+//    List.Nil -> 0
+//    is List.Cons -> intList.head + sum(intList.tail)
+//}
 
-    fun setHead(newHead: A): List<A> = when (this) {
+sealed class List<out A> {
+    abstract fun isEmpty(): Boolean
+    fun cons(newItem: @UnsafeVariance A): List<A> = Cons(newItem, this)
+
+    fun setHead(newHead: @UnsafeVariance A): List<A> = when (this) {
         is Nil -> throw IllegalArgumentException()
         is Cons -> tail.cons(newHead)
     }
 
-    fun drop(n: Int): List<A> {
-        tailrec fun drop_(list: List<A>, nn: Int): List<A> = when {
-            nn == 0 -> list
-            list is Cons -> drop_(list.tail, nn - 1)
-            list is Nil -> list
-            else -> throw IllegalArgumentException()
-        }
+    fun drop(n: Int): List<A> = drop(this, n)
+    fun dropWhile(p: (A) -> Boolean): List<A> = dropWhile(this, p)
+    fun reverse(): List<A> = reverse(invoke(), this)
+    fun init(): List<A> = this.reverse().drop(1).reverse()
 
-        return drop_(this, n)
-    }
 
     private object Nil : List<Nothing>() {
         override fun isEmpty() = true
@@ -42,8 +50,7 @@ sealed class List<A> {
     class Cons<A>(
         internal val head: A,
         internal val tail: List<A>,
-    ) :
-        List<A>() {
+    ) : List<A>() {
         override fun isEmpty(): Boolean = false
         override fun toString(): String = "[${toString("", this)}NIL]"
 
@@ -59,5 +66,22 @@ sealed class List<A> {
         operator
         fun <A> invoke(vararg az: A): List<A> = az.foldRight(Nil as List<A>)
         { a: A, list: List<A> -> Cons(a, list) }
+
+        tailrec fun <A> drop(list: List<A>, n: Int): List<A> = when (list) {
+            Nil -> list
+            is Cons -> if (n <= 0) list else drop(list.tail, n - 1)
+        }
+
+        tailrec fun <A> dropWhile(list: List<A>, p: (A) -> Boolean): List<A> =
+            when (list) {
+                Nil -> list
+                is Cons -> if (!p(list.head)) list else dropWhile(list.tail, p)
+            }
+
+        tailrec fun <A> reverse(acc: List<A>, list: List<A>): List<A> =
+            when (list) {
+                Nil -> acc
+                is Cons -> reverse(acc.cons(list.head), list.tail)
+            }
     }
 }
